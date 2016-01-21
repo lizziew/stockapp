8b83c783164b78fd6112d44b9c3f79873f951a18
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2016-01-21 16:32:29 -0500

    live updating apple

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express');  
+var path = require('path'); 
+var favicon = require('serve-favicon');  
+var logger = require('morgan'); 
+var cookieParser = require('cookie-parser');  
+var bodyParser = require('body-parser'); 
+
+var yahooFinance = require('yahoo-finance');  
+var util = require('util');  
+
+require('colors');  
+var _ = require('lodash');  
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -55,6 +61,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var yahooFinance = require('yahoo-finance');
+require('colors');  
+var _ = require('lodash');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Stock Analysis App' });
+});
+
+//SINGLE COMPANY : LAST TRADED WITH PRICE ONLY
+router.get('/getSingleData', function(req, res) {
+	var FIELDS = _.flatten([
+	  ['l1']
+	]);
+
+	var SYMBOL = 'AAPL';
+
+	yahooFinance.snapshot({
+		fields: FIELDS,
+		symbol: SYMBOL
+	}, function (err, snapshot) {
+	if (err) { throw err; }
+	console.log(util.format('=== %s ===', SYMBOL).cyan);
+	console.log(JSON.stringify(snapshot));
+	res.send(JSON.stringify(snapshot));
+	});
 });
 
 module.exports = router;
